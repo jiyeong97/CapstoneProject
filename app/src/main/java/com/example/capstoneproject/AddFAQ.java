43e2fe7dd693b07d0d6a6a2d0769f7ba059f1d25
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2023-03-22 00:33:48 -0600

    Data and button update2 -Jiyeong

--- app/src/main/java/com/example/capstoneproject/AddFAQ.java
+++ app/src/main/java/com/example/capstoneproject/AddFAQ.java
@@ -3,6 +3,7 @@ package com.example.capstoneproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddFAQ extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AddFAQ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_faq2);
+    }
+
+    public void goBackToPreviousPage(View view){
+        finish();
     }
 }
